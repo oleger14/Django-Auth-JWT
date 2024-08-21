4af943e21015f96399f0aefba4bc5b56e00a9b48
Oleg <oleg.tcm@gmail.com>
2024-08-21 16:30:01 +0300

    21.08

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -31,19 +31,20 @@ class Post(models.Model):
     objects = models.Manager() # менеджер, применяемый по умолчанию
     published = PublishedManager() # конкретно-прикладной менеджер
 
-class Meta:
- ordering = ['-publish']
- indexes = [
-    models.Index(fields=['-publish'])
- ]
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
 
-def __str__(self):
- return self.title
+    def __str__(self):
+        return self.title
 
-def get_absolute_url(self):
-   return reverse('blog:post_detail',
-                  args=[self.publish.year,
-                        self.publish.month,
-                        self.publish.day,
-                        self.slug])
+    def get_absolute_url(self):
+            return reverse('blog:post_detail',
+                            args=[self.publish.year,
+                                    self.publish.month,
+                                    self.publish.day,
+                                    self.slug])
+
 # Create your models here.
